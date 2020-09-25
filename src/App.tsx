8700562cf740f5aad8aef77ce3d236efd0c4f27c
Silla Tan <silla.tan@gmail.com>
2020-09-26 02:10:44 +1000

    Add simple form to add users

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,42 @@
-import React from "react";
-import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { TextField, List, ListItem } from "@material-ui/core";
 
-export const App = () => <Button color="primary">Hello World</Button>;
+const UserForm = ({ addUser }) => {
+  const [name, setName] = useState("");
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        addUser(name);
+        setName("");
+      }}
+    >
+      <TextField
+        label="Name"
+        variant="outlined"
+        value={name}
+        onChange={(e) => setName(e.target.value || "")}
+      />
+    </form>
+  );
+};
+
+const UserList = ({ users }) => (
+  <List>
+    {users.map((user, i) => (
+      <ListItem key={`user-${i}`}>{user}</ListItem>
+    ))}
+  </List>
+);
+
+export const App = () => {
+  const [users, setUser] = useState<string[]>([]);
+
+  return (
+    <>
+      <UserForm addUser={(user) => setUser(users.concat(user))} />
+      <UserList users={users} />
+    </>
+  );
+};
